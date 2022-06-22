a8feedb3fdb44c7802ffcec24754102a84a581d1
shanayR <88942459+shanayR@users.noreply.github.com>
2022-06-22 11:31:31 +0500

    Update loginBanner.js

--- frontend/cryptobit/src/components/login/loginBanner.js
+++ frontend/cryptobit/src/components/login/loginBanner.js
@@ -3,7 +3,7 @@ import React from "react";
 function LoginBanner(){
     return(
         
-            <div className="d-flex space-around items-centre">
+            
                 <div className="form-text" data-aos="fade-right" data-aos-easing="linear" data-aos-duration="1500">
                     <div className="welcome-heading">
                         <h4>welcome</h4>
@@ -14,7 +14,7 @@ function LoginBanner(){
                             Easily</h3>
                     </div>
                 </div>
-            </div>
+            
         
 )
 }
