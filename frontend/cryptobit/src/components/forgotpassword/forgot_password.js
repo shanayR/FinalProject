5eb45af0b94b340342fed6b3f4a8430ba281e5b8
shanayR <88942459+shanayR@users.noreply.github.com>
2022-06-22 11:30:10 +0500

    Update forgot_password.js

--- frontend/cryptobit/src/components/forgotpassword/forgot_password.js
+++ frontend/cryptobit/src/components/forgotpassword/forgot_password.js
@@ -1,29 +1,17 @@
-import React from "react"
-import ForgotForm from "./forgotForm"
+import React from "react";
+import ForgotForm from "./forgotForm";
+import ForgotBanner from "./forgotBanner";
 
 function Forgot(){
     return(
         <section id="forgotpass-section-1" class="darkblue form-section">
-        <div id="share-bar"></div>
-        <div class="container">
-            <div class="d-flex space-around items-centre">
-                <div class="form-text" data-aos="fade-right" data-aos-easing="linear" data-aos-duration="1500">
-                    <div class="welcome-heading">
-                        <h4>welcome</h4>
-                    </div>
-                    <div class="form-description">
-                        <h3>Set New Password To
-                            Login Easily</h3>
-                    </div>
-                </div>
-        <div class="main-form top-border-form" data-aos="fade-up" data-aos-easing="linear" data-aos-duration="1500">
-            <div class="form-blue-box blue-box">
-        <h2>NEW PASSWORD</h2>
-          <ForgotForm/>
-        </div>       
-      </div>  
-      </div>  
-      </div> 
+            <div id="share-bar"></div>
+            <div class="container">
+                <div class="d-flex space-around items-centre">
+                    <ForgotBanner/>
+                    
+                </div>  
+            </div> 
     </section>
     )
 }
