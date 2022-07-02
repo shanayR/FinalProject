a435b51e1d59c027625f4d28149988f4efeafdd6
elsazehra2002@gmail.com <elsazehra2002@gmail.com>
2022-07-02 21:15:16 +0500

    Form Validations

--- frontend/cryptobit/src/components/admin/add_admin.js
+++ frontend/cryptobit/src/components/admin/add_admin.js
@@ -1,30 +1,71 @@
-import React, { useEffect } from "react";
+import React, { useState,useEffect } from "react";
 import axios from "axios";
+import SocialShare from "../common/social-share";
 
 
 function Add(){
 
-    const submitAdd = (event)=>{
-        event.preventDefault();
-        var bodyFormData = new FormData();
-        bodyFormData.append('name', document.querySelector('[name="name"]').value);
-        bodyFormData.append('price',parseFloat(document.querySelector('[name="price"]').value));
-        bodyFormData.append('coinLogo', "file.jpg");
+    // const submitAdd = (event)=>{
+    //     event.preventDefault();
+    //     var bodyFormData = new FormData();
+    //     bodyFormData.append('name', document.querySelector('[name="name"]').value);
+    //     bodyFormData.append('price',parseFloat(document.querySelector('[name="price"]').value));
+    //     bodyFormData.append('coinlogo', "file.jpg");
         
-        axios({
-            method: "post",
-            headers: { 'content-type': 'application/json' },
-            url: "http://localhost:8080/add",
-            data: bodyFormData,
-          }).then(function (response) {
-              //handle success
-              console.log(response);
-                return response;
+    //     axios({
+    //         method: "post",
+    //         headers: { 'content-type': 'application/json' },
+    //         url: "http://localhost:8080/add",
+    //         data: bodyFormData,
+    //       }).then(function (response) {
+    //           //handle success
+    //           console.log(response);
+    //             return response;
 
-            }).catch(function (response) {
-              //handle error
-              console.log(response);
-            });
+    //         }).catch(function (response) {
+    //           //handle error
+    //           console.log(response);
+    //         });
+    // }
+    const initialValues = {name:"", price:"", coinLogo:""}
+    const [formValues, setFormValues] = useState(initialValues)
+    const [formErrors, setFormErrors] = useState({})
+    const [isSubmit , setIsSubmit] = useState(false)
+
+    const handleChange = (e) => {
+        // console.log(e.target);
+        const { name , value } = e.target;
+        setFormValues({...formValues,[name] : value})
+        console.log(formValues);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setFormErrors(validate(formValues))
+        setIsSubmit(true)
+    };
+
+    useEffect(() =>{
+        console.log(formErrors);
+        if(Object.keys(formErrors).legth === 0 && isSubmit){
+            console.log(formValues);
+        }
+    })
+
+    const validate =(values) =>{
+        const errors = {}
+        if(!formValues.name){
+            errors.name = "name is required"
+        }
+        if(!formValues.price){
+            errors.price = "Phone Number is required"
+        }
+        if(!formValues.coinLogo){
+            errors.coinLogo = "Image is required"
+        }
+       
+        return errors
+
     }
     // useEffect(() => {
     //     // POST request using axios inside useEffect React hook
@@ -37,27 +78,61 @@ function Add(){
    
 
     return(
-        <section  id="home-section-7" className="section-padding hundred navyblue">
+        <section  id="home-section-7" className="darkblue form-section">
+           <SocialShare />
             <div className="container">
-            <div className="d-flex add-heading ">
-                <h2 className="add-heading fontsize40 fontweight400 margin-bottom80 white-text"
-                    data-aos="fade-up" data-aos-duration="1000">Add coins</h2>
-            </div>
-            <div className="form-sec">
-            <form onSubmit={submitAdd} id="add_form" method="post" encType="multipart/form-data" data-aos="fade-up" data-aos-duration="1000"data-aos-delay="100">
-                <div className="dflex center space-around width100 flex-column subscribe-form">
-                    {/* <label className="white-text">Coin Name:</label>  */}
-                    <input type="text" name="name" className="darkblue margin-bottom26 capitalize" placeholder="Name" /><br />
-                    {/* <label className="white-text">Coin Price: $</label> */}
-                    <input type="number" step="0.01" name="price"  className="darkblue margin-bottom26 capitalize" placeholder="Price"/><br />
-                    {/* <label className="white-text">Coin Logo: </label> */}
-                    <input type="file" name="coinLogo"  className="darkblue margin-bottom26 capitalize" placeholder="Coin Logo"/><br />
-                    <input type="submit" className="button" />
+            <div class="d-flex  items-centre ">
+            <div class="form-text" data-aos="fade-right" data-aos-easing="linear" data-aos-duration="1500">
+                <div class="update-heading">
+                    <h4>Add Coins</h4>
                 </div>
-            </form>
             </div>
+            <div className="main-form form-middle top-border-form" data-aos="fade-up" data-aos-easing="linear" data-aos-duration="1500">
+                <div class="form-blue-box blue-box">
+                    <form  onsubmit={handleSubmit} action="" method="POST">
+                        <div className="form">
+                                <label for="" className="form-label">Coin</label>
+                                <input 
+                                    type="text" 
+                                    name="name" 
+                                    value={formValues.name}  
+                                    className="form-control" 
+                                    onChange={handleChange}
+                                />
+                                <p className="error">{formErrors.name}</p>
+                            
+                                <label for="" className="form-label">Price</label>
+                                <input 
+                                    type="number" 
+                                    step="0.01" 
+                                    name="price" 
+                                    value={formValues.price}
+                                    className="form-control" 
+                                    onChange={handleChange}
+                                />
+                                <p className="error">{formErrors.price}</p>
+                
+                                <label for="" className="form-label">Coin Logo</label>
+                                <input 
+                                    type="file" 
+                                    name="coinlogo" 
+                                    className="form-control" 
+                                    onChange={handleChange}
+                                    value={formValues.coinlogo}
+                                    
+                                />
+                                <p className="error">{formErrors.coinLogo}</p>
+                            
+                            
+                                <input type="submit" value="Submit" className="button" />
+                            
+                        </div>
+                    </form>
+                </div>
+                </div>
             </div>
-        </section>
+        </div>
+    </section>
     )
 }
 
--- frontend/cryptobit/src/components/register/registerForm.js
+++ frontend/cryptobit/src/components/register/registerForm.js
@@ -1,26 +1,86 @@
-import React from "react"
+import React , {useState , useEffect} from "react"
 import {Form} from "react-bootstrap"
 import {Button } from "react-bootstrap"
 
 
 function RegisterForm(){
+    const initialValues = {email:"", password:"",accountNumber:"",regConfirmPassword:""}
+    const [formValues, setFormValues] = useState(initialValues)
+    const [formErrors, setFormErrors] = useState({})
+    const [isSubmit , setIsSubmit] = useState(false)
+
+    const handleChange = (e) => {
+        // console.log(e.target);
+        const { name , value } = e.target;
+        setFormValues({...formValues,[name] : value})
+        console.log(formValues);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setFormErrors(validate(formValues))
+        setIsSubmit(true)
+    };
+
+    useEffect(() =>{
+        console.log(formErrors);
+        if(Object.keys(formErrors).legth === 0 && isSubmit){
+            console.log(formValues);
+        }
+    })
+
+    const validate =(values) =>{
+        const errors = {}
+        const regex =  /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        if(!formValues.password){
+            errors.password = "Password is required"
+        }else if(formValues.password.length < 4){
+            errors.password = "Password must be more than 4 characters"
+        }else if(formValues.password.length > 10){
+            errors.password = "Password cannot exceed more than 10 characters"
+        }
+        if(!formValues.email){
+            errors.email = "Email is required"
+        }else if(!regex.test(values.email)){
+            errors.email ="Enter correct Email"
+        }
+        if(!formValues.regConfirmPassword){
+            errors.regConfirmPassword = "Re-enter your password"
+        }
+        if(!formValues.accountNumber){
+            errors.accountNumber = "Account Number is required"
+        }else if(formValues.accountNumber.length < 14){
+            errors.accountNumber = "Account Number must be 14 digits"
+        }
+    
+
+        return errors
+
+    }
     return(
         <div className="main-form top-border-form" data-aos="fade-up" data-aos-easing="linear" data-aos-duration="1500">
             <div className="form-blue-box blue-box">
                 <h2>REGISTER</h2>
-                <Form>
+                {Object.keys(formErrors).length === 0 && isSubmit && (
+                <div className="white-text margin-bottom26">Registered Successfully!</div>
+            )}
+                <Form onSubmit={handleSubmit}>
                     <Form.Group id="registerForm" className="form" controlId="formBasicEmail">
                         <Form.Label id="regemail">Email address</Form.Label>
-                        <Form.Control type="email"  />
+                        <Form.Control type="email" name="email" onChange={handleChange}/>
+                        <p className="error">{formErrors.email}</p>
                         
                         <Form.Label>Account Number</Form.Label>
-                        <Form.Control type="number" name="accountNumber" id="regaccnum"/>
+                        <Form.Control type="number" name="accountNumber" id="regaccnum" onChange={handleChange}/>
+                        <p className="error">{formErrors.accountNumber}</p>
 
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" name="password" id="regpass"/>
+                        <Form.Control type="password" name="password" id="regpass" onChange={handleChange}/>
+                        <p className="error">{formErrors.password}</p>
                         
                         <Form.Label>Confirm Password</Form.Label>
-                        <Form.Control type="password"name="regConfirmPassword"  id="regpass"/>
+                        <Form.Control type="password"name="regConfirmPassword"  id="regpass" onChange={handleChange}/>
+                        <p className="error">{formErrors.regConfirmPassword}</p>
                         <input variant="primary" type="submit" value="Register"  className="button" />
                     
                     </Form.Group>
