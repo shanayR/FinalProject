5b7fa1cf5c2221aceb649f5db4c60fe0c9f1ddb8
shane.ruqayya <shane.ruqayya@genetechsolutions.com>
2022-07-15 09:02:50 +0500

    -

--- backend/index.js
+++ backend/index.js
@@ -21,10 +21,10 @@ app.use(express.urlencoded({extended:true}));
 
 app.use(express.json())
 app.use(cors(
-  {
-    origin: "http://localhost:3000", // <-- location of the react app were connecting to
-    credentials: true,
-  }
+  // {
+  //   origin: "http://localhost:3000", // <-- location of the react app were connecting to
+  //   credentials: true,
+  // }
 ))
 app.set('view engine', 'ejs');
 
