179bbad674af8bc13fc9c95f10832b24a60fe5a0
shanayR <88942459+shanayR@users.noreply.github.com>
2022-07-15 18:37:21 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,6 +15,13 @@ const port = process.env.API_PORT;
 // const __dirname = path.resolve(); 
 dbConnection()
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 
 app.use(cors())
 app.use(express.urlencoded({extended:false}));
